529888f2d6731f1e1f76ad3798fdcecf2a11adad
Sinisa <sinisa.becic@outlook.com>
2022-01-25 19:10:10 +0100

    Deleted

--- database/migrations/2022_01_25_153652_add_is_active_to_users.php
+++ database/migrations/2022_01_25_153652_add_is_active_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnDeletedAtToUsers extends Migration
+class AddIsActiveToUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,7 @@ class AddColumnDeletedAtToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
-            $table->softDeletes();
+            $table->integer('is_active')->default(0);
         });
     }
 
@@ -27,8 +26,7 @@ class AddColumnDeletedAtToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
-            $table->dropColumn('deleted_at');
+            $table->dropColumn('is_active');
         });
     }
 }
