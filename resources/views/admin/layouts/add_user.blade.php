a339b698018d8a44c0baf7580646dd73ffcd4a69
Sinisa <sinisa.becic@outlook.com>
2022-02-12 10:48:35 +0100

    Added attributes(data-url) in bulk buttons

--- resources/views/admin/layouts/add_user.blade.php
+++ resources/views/admin/layouts/add_user.blade.php
@@ -9,7 +9,8 @@
             </div>
             <div class="modal-body">
 
-                <form method="POST" action="" id="addUserForm" enctype="multipart/form-data">
+                <form method="POST" action="" id="addUserForm" data-url="{{ route('users.store') }}"
+                      enctype="multipart/form-data">
                     @csrf
                     @method('POST')
 
