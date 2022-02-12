1599f90bd5d365086ea60780134b256cdf4f30eb
Sinisa <sinisa.becic@outlook.com>
2022-02-12 11:03:32 +0100

    Added attributes(data-url) in bulk buttons and removed functions from blade file

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,7 +3,8 @@
 
 @section('content')
 
-    <form method="POST" action="" enctype="multipart/form-data" id="addPostForm">
+    <form method="POST" action="" enctype="multipart/form-data" id="addPostForm"
+          data-url="{{ route('blog.posts.store') }}">
         @csrf
         @method('POST')
         <div class="form-group">
@@ -87,104 +88,6 @@
 @section('script')
     <script src="https://cdn.tiny.cloud/1/aq4yxpek36fz7epa0kqorg4304hgfjrk8qqwtf0binzc8iw6/tinymce/5/tinymce.min.js"
             referrerpolicy="origin"></script>
-    <script>
-
-        //? Dodavanje korisnika
-        $(document).ready(function () {
-
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-
-            //? Za dodavanje korisnika
-            $('#addPostForm').submit(function (e) {
-                e.preventDefault();
-
-                var formData = new FormData(this);
-
-                $.ajax({
-                    method: 'POST',
-                    url: "{{ route('blog.posts.store') }}",
-                    data: formData,
-                    success: function () {
-                        clearFields('#addPostForm');
-                        Swal.fire({
-                            title: 'Post created!',
-                            // text: '',
-                            icon: 'success',
-                            toast: true,
-                            position: 'top-right',
-                            showConfirmButton: false,
-                            timer: 2500,
-                        });
-                    },
-                    error: function () {
-                        // alert('Greska! Pokusaj ponovo');
-                        Swal.fire({
-                            title: 'Error! Something went wrong',
-                            // text: '',
-                            icon: 'error',
-                            toast: true,
-                            position: 'top-right',
-                            showConfirmButton: false,
-                            timer: 2500,
-                        })
-                    },
-                    contentType: false,
-                    processData: false,
-                });
-            });
-        });
-
-        function clearFields(form) {
-            $(':input', form)
-                .not(':button, :submit, :reset, :hidden')
-                .val('')
-                .prop('checked', false)
-                .prop('selected', false);
-
-        }
-
-        //? Editor
-        var editor_config = {
-            path_absolute: "/",
-            selector: 'textarea.my-editor',
-            relative_urls: false,
-            plugins: [
-                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-                "searchreplace wordcount visualblocks visualchars code fullscreen",
-                "insertdatetime media nonbreaking save table directionality",
-                "emoticons template paste textpattern"
-            ],
-            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
-            file_picker_callback: function (callback, value, meta) {
-                var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-                var y = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-
-                var cmsURL = editor_config.path_absolute + 'filemanager?editor=' + meta.fieldname;
-                if (meta.filetype == 'image') {
-                    cmsURL = cmsURL + "&type=Images";
-                } else {
-                    cmsURL = cmsURL + "&type=Files";
-                }
-
-                tinyMCE.activeEditor.windowManager.openUrl({
-                    url: cmsURL,
-                    title: 'Filemanager',
-                    width: x * 0.8,
-                    height: y * 0.8,
-                    resizable: "yes",
-                    close_previous: "no",
-                    onMessage: (api, message) => {
-                        callback(message.content);
-                    }
-                });
-            }
-        };
-
-        tinymce.init(editor_config);
-
-    </script>
+    <script src="{{ asset('/js/admin/posts/functions.js') }}"></script>
+    <script src="{{ asset('/js/admin/posts/editor.js') }}"></script>
 @endsection
